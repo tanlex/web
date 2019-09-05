dc3cde275e9e08cf1d648115ea25d5722099ed8f
tanlex <523249435@qq.com>
2019-09-05 18:08:59 +0800

    jwt

--- jwt.php
+++ jwt.php
@@ -10,74 +10,98 @@ require __DIR__.'/vendor/autoload.php';
 use \Firebase\JWT\JWT;
 
 
-/*
-$key = "example_key";
-$token = array(
-    "iss" => "http://example.org",
-    "aud" => "http://example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
-);
+class Myjwt{
 
 
-$jwt = JWT::encode($token, $key);
-$decoded = JWT::decode($jwt, $key, array('HS256'));
+    private $key = 'kyen.xjjglkgn';
 
-print_r($decoded);
+    /**
+     * 创建Token
+     */
+    public function getToken(){
+        $key = $this->key; //key
+        $time = time(); //当前时间
+        $token = [
+            'iss' => 'http://www.helloweba.net', //签发者 可选
+            'aud' => 'http://www.helloweba.net', //接收该JWT的一方，可选
+            'iat' => $time, //签发时间
+            'nbf' => $time, //(Not Before)：某个时间点后才能访问，比如设置time+30，表示当前时间30秒后才能使用
+            'exp' => $time+7200, //过期时间,这里设置2个小时
+            'data' => [ //自定义信息，不要定义敏感信息
+                'userid' => 1,
+                'username' => '李小龙'
+            ]
+        ];
+        echo JWT::encode($token, $key); //输出Token
+    }
+
+    /**
+     * 解析Token
+     */
+    public function getInfoByToken(){
+        $key = $this->key; //key要和签发的时候一样
+
+        $jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93d3cuaGVsbG93ZWJhLm5ldCIsImF1ZCI6Imh0dHA6XC9cL3d3dy5oZWxsb3dlYmEubmV0IiwiaWF0IjoxNTY3NjczNjQ0LCJuYmYiOjE1Njc2NzM3MDQsImV4cCI6MTU2NzY4MDg0NCwiZGF0YSI6eyJ1c2VyaWQiOjEsInVzZXJuYW1lIjoiXHU2NzRlXHU1YzBmXHU5Zjk5In19.S0HniEpDComOo5oYEZlAMCpByyAYQ6x9yp3rPstvpM0"; //签发的Token
+        try {
+            JWT::$leeway = 60;//当前时间减去60，把时间留点余地
+            $decoded = JWT::decode($jwt, $key, ['HS256']); //HS256方式，这里要和签发的时候对应
+            $arr = (array)$decoded;
+            print_r($arr);
+
+        } catch(\Firebase\JWT\SignatureInvalidException $e) {  //签名不正确
+            echo $e->getMessage();
+        }catch(\Firebase\JWT\BeforeValidException $e) {  // 签名在某个时间点之后才能用
+            echo $e->getMessage();
+        }catch(\Firebase\JWT\ExpiredException $e) {  // token过期
+            echo $e->getMessage();
+        }catch(Exception $e) {  //其他错误
+            echo $e->getMessage();
+        }
+    }
 
 
-$decoded_array = (array) $decoded;
+    /**
+     * 实战创建Token
+     */
+    public function authorizations()
+    {
+        $key = $this->key; //key
+        $time = time(); //当前时间
+
+        //公用信息
+        $token = [
+            'iss' => 'http://www.helloweba.net', //签发者 可选
+            'iat' => $time, //签发时间
+            'data' => [ //自定义信息，不要定义敏感信息
+                'userid' => 1,
+            ]
+        ];
+
+        $access_token = $token;
+        $access_token['scopes'] = 'role_access'; //token标识，请求接口的token
+        $access_token['exp'] = $time+7200; //access_token过期时间,这里设置2个小时
+
+        $refresh_token = $token;
+        $refresh_token['scopes'] = 'role_refresh'; //token标识，刷新access_token
+        $refresh_token['exp'] = $time+(86400 * 30); //access_token过期时间,这里设置30天
+
+        $jsonList = [
+            'access_token'=>JWT::encode($access_token,$key),
+            'refresh_token'=>JWT::encode($refresh_token,$key),
+            'token_type'=>'bearer' //token_type：表示令牌类型，该值大小写不敏感，这里用bearer
+        ];
+        Header("HTTP/1.1 201 Created");
+        echo json_encode($jsonList); //返回给客户端token信息
+    }
 
 
-JWT::$leeway = 60; // $leeway in seconds
-$decoded = JWT::decode($jwt, $key, array('HS256'));
 
-*/
+}
 
-$privateKey = <<<EOD
------BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
-vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
-5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
-AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
-bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
-Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
-cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
-5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
-ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
-k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
-qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
-eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
-B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
------END RSA PRIVATE KEY-----
-EOD;
+$myjwt = new Myjwt();
 
-$publicKey = <<<EOD
------BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
-4i2p/llLCrEeQhta5kaQu/RnvuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t
-0tyazyZ8JXw+KgXTxldMPEL95+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4
-ehde/zUxo6UvS7UrBQIDAQAB
------END PUBLIC KEY-----
-EOD;
+//$myjwt->getToken();
 
-$token = array(
-    "iss" => "example.org",
-    "aud" => "example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
-);
+//$myjwt->getInfoByToken();
 
-$jwt = JWT::encode($token, $privateKey, 'RS256');
-echo "Encode:\n" . print_r($jwt, true) . "\n";
-
-$decoded = JWT::decode($jwt, $publicKey, array('RS256'));
-
-/*
- NOTE: This will now be an object instead of an associative array. To get
- an associative array, you will need to cast it as such:
-*/
-
-$decoded_array = (array) $decoded;
-echo "Decode:\n" . print_r($decoded_array, true) . "\n";
-
+$myjwt->authorizations();
